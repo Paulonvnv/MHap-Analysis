ceca33bcc1e33dc3242b4352df304cc0f661de98
Paulo Manrique <paulonvnv@gmail.com>
2022-11-02 11:38:47 -0400

    Create fx_get_polygenomic.R
    Function to identify polyclonal samples (samples where multiple parasites haplotypes coexist). Phasing is based on the read counts of each observed allele in the heterozygous loci.

---
+++ fx_get_polygenomic.R
@@ -0,0 +1,78 @@
+
+fx_get_polygenomic = function(ampseq_object, strata, update_popsummary = T){
+  
+  library(Hmisc)
+  
+  ampseq_loci_abd_table = ampseq_object[["loci_abd_table"]]
+  metadata = ampseq_object[["metadata"]]
+  loci_performance = ampseq_object[["loci_performance"]]
+  
+  polygenomic = NULL
+  
+  for(sample in rownames(ampseq_loci_abd_table)){
+    polygenomic = rbind(polygenomic, data.frame(NPolyLoci = sum(grepl("_",ampseq_loci_abd_table[sample, ])),
+               Polyloci = paste(names(ampseq_loci_abd_table[sample, ])[which(grepl("_",ampseq_loci_abd_table[sample, ]))], collapse = "/"),
+               alleles_at_loci = paste(ampseq_loci_abd_table[sample, ][which(grepl("_",ampseq_loci_abd_table[sample, ]))], collapse = "/"),
+               nalleles_per_loci = paste(
+                 sapply(
+                   sapply(
+                     ampseq_loci_abd_table[sample, ][which(grepl("_", ampseq_loci_abd_table[sample, ]))], function(x){strsplit(x, "_")}),
+                   function(x) length(x)),
+                 collapse = "/"),
+               coi = ifelse(sum(grepl("_",ampseq_loci_abd_table[sample, ])) == 0,
+                            1,
+                            max(unlist(sapply(
+                              sapply(
+                                ampseq_loci_abd_table[sample, ][which(grepl("_",ampseq_loci_abd_table[sample, ]))], function(x){strsplit(x, "_")}),
+                              function(x) length(x)))))))
+  }
+  
+  pop_summary = data.frame(
+    pop = "Total",
+    n = nrow(polygenomic),
+    mean_coi = mean(polygenomic[["coi"]]),
+    n_poly = nrow(polygenomic[polygenomic[["coi"]] > 1,]),
+    prop_poly = binconf(x = nrow(polygenomic[polygenomic[["coi"]] > 1,]),
+                        n = nrow(polygenomic),
+                        method = "exact")[1],
+    prop_poly_lower = binconf(x = nrow(polygenomic[polygenomic[["coi"]] > 1,]),
+                              n = nrow(polygenomic),
+                              method = "exact")[2],
+    prop_poly_upper = binconf(x = nrow(polygenomic[polygenomic[["coi"]] > 1,]),
+                              n = nrow(polygenomic),
+                              method = "exact")[3])
+  
+  
+  for(pop in levels(as.factor(metadata[[strata]]))){
+    pop_summary = rbind(pop_summary,
+                        data.frame(
+                          pop = pop,
+                          n = nrow(polygenomic[metadata[[strata]] == pop,]),
+                          mean_coi = mean(polygenomic[metadata[[strata]] == pop,][["coi"]]),
+                          n_poly = nrow(polygenomic[metadata[[strata]] == pop & polygenomic[["coi"]] > 1,]),
+                          prop_poly = binconf(x = nrow(polygenomic[metadata[[strata]] == pop & polygenomic[["coi"]] > 1,]),
+                                              n = nrow(polygenomic[metadata[[strata]] == pop,]),
+                                              method = "exact")[1],
+                          prop_poly_lower = binconf(x = nrow(polygenomic[metadata[[strata]] == pop & polygenomic[["coi"]] > 1,]),
+                                                    n = nrow(polygenomic[metadata[[strata]] == pop,]),
+                                                    method = "exact")[2],
+                          prop_poly_upper = binconf(x = nrow(polygenomic[metadata[[strata]] == pop & polygenomic[["coi"]] > 1,]),
+                                                    n = nrow(polygenomic[metadata[[strata]] == pop,]),
+                                                    method = "exact")[3]))
+  }
+  
+  # Defining the proportion of polygenomic infections detected by loci --
+  
+  loci_performance[["prop_poly_detected"]] = apply(ampseq_loci_abd_table, 2, function(x) sum(grepl("_",x))/length(x))
+  
+  if(update_popsummary){
+    ampseq_object[["metadata"]] = cbind(ampseq_object[["metadata"]], polygenomic)
+    ampseq_object[["loci_performance"]] = loci_performance
+    ampseq_object[["pop_summary"]] = if(is.null(ampseq_object[["pop_summary"]])){pop_summary}else{cbind(ampseq_object[["pop_summary"]], pop_summary)}
+    return(ampseq_object)
+  }else{
+    return(pop_summary)
+  }
+  
+}
+
