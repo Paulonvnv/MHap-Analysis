6070b53cb58aff1e65342239c3fa033bae1d8474
Paulo Manrique <paulonvnv@gmail.com>
2022-11-02 11:24:30 -0400

    Create fx_read_cigar_tables.R
    Function to read cigar format tables in R. The output is a cigar object class containing the cigar table and a metadata table

---
+++ fx_read_cigar_tables.R
@@ -0,0 +1,50 @@
+
+fx_read_cigar_tables = function(paths, sample_id_pattern){
+  
+  # Uploading data sets and merge CIGARs tables
+  # and generate a metadata table for sample names, runs and sample types (controls vs samples of interest)
+  cigar_tables = NULL
+  metadata = NULL
+  
+  for(run in list.files(paths)){
+    cigar_run = read.table(file.path(paths, run, "dada2/run_dada2/CIGARVariants.out.tsv"))
+    names(cigar_run) = cigar_run[1,]
+    samples = sapply(strsplit(cigar_run[-1,1], "_"), function(x) x[1])
+    samples[!grepl(sample_id_pattern,samples)] <- paste(samples[!grepl(sample_id_pattern,samples)], run, sep = "_")
+    cigar_run = cigar_run[-1, -1]
+    cigar_run = apply(cigar_run, 2, function(x) as.integer(x))
+    rownames(cigar_run) = samples
+    cigar_tables[[run]] = cigar_run
+    metadata = rbind(metadata, data.frame(samples = samples, run = run, typeofSamp = NA))
+    metadata[grepl(sample_id_pattern, metadata[["samples"]]),][["typeofSamp"]] = "Samples"
+    metadata[!grepl(sample_id_pattern, metadata[["samples"]]),][["typeofSamp"]] = "Controls"
+    rm(list = c("run", "cigar_run", "samples"))
+  }
+  
+  # Combining cigar tables
+  
+  cigar_table = NULL
+  
+  for(run in names(cigar_tables)){
+    temp_cigar_table = data.frame(samples = rownames(cigar_tables[[run]]), cigar_tables[[run]])
+    names(temp_cigar_table) = c("samples", colnames(cigar_tables[[run]]))
+    temp_cigar_table %<>% pivot_longer(cols = all_of(names(temp_cigar_table)[-1]), names_to = "alleles", values_to = "counts")
+    cigar_table = rbind(cigar_table, temp_cigar_table)
+    rm(run)
+  }
+  
+  cigar_table %<>% pivot_wider(names_from = "alleles", values_from = "counts", values_fn = first)
+  sample_names = cigar_table[, 1]
+  cigar_table = cigar_table[, -1]
+  cigar_table = as.matrix(cigar_table)
+  
+  rownames(cigar_table) = unlist(sample_names)
+  
+  cigar_table[is.na(cigar_table)] = 0
+  
+  metadata = metadata[!duplicated(metadata$samples),]
+  
+  return(list(cigar_table = cigar_table,
+              metadata = metadata))
+  
+}
