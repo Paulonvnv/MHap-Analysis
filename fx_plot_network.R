f28fa15c8a04644d20ffe4670e0fe3fb81a79b56
Paulo Manrique <paulonvnv@gmail.com>
2022-11-02 11:54:45 -0400

    Create fx_plot_network.R
    Function to generate a network plot of  pairwise relatedness or other pairwise correlation matrices

---
+++ fx_plot_network.R
@@ -0,0 +1,48 @@
+
+fx_plot_network = function(pairwise_long_matrix,
+                        variable,
+                        cols,
+                        threshold,
+                        metadata,
+                        sample_id,
+                        group_by,
+                        levels,
+                        colors){
+  
+  library(igraph)
+  
+  edges = NULL
+  
+  selected_edges <- pairwise_long_matrix[pairwise_long_matrix[[variable]] >= threshold,][,cols]
+  for(edge in 1:nrow(selected_edges)){
+    edges = c(edges,unlist(selected_edges[edge,]))
+  }
+  
+  isolates = unique(unlist(pairwise_long_matrix[,cols]))[!(unique(unlist(pairwise_long_matrix[,cols])) %in% unique(edges))]
+  
+  network_object = graph(edges = edges, isolates = isolates, directed=F)
+  
+  node_colors = c(unique(edges), isolates)
+  
+  names(node_colors) = node_colors
+  
+  for(level in 1:length(levels)){
+    node_colors[node_colors %in% metadata[metadata[[group_by]] == levels[level],][[sample_id]]] = colors[level]
+  }
+  
+  plot_network = plot.igraph(network_object,
+              vertex.color = node_colors,
+              vertex.size=3,
+              vertex.label.cex=0,
+              vertex.label.dist=0.5, 
+             # vertex.label.degree=-pi/2,
+              vertex.label.color="black",
+              vertex.label=NA,
+              asp=0,
+              xlim=c(-1,1),
+              ylim=c(-1,1))
+  
+  return(list(network_object = network_object,
+              plot_network = plot_network))
+  
+}
