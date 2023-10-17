bbe13f208dc674e445c3550e4bd51f8ddd7e3d64
Paulo Manrique <paulonvnv@gmail.com>
2023-10-17 00:58:06 -0400

    adding Pvivax data

---
+++ docs/data/Pvivax/asv2cigar_pipeline_PvGTSeq.sh
@@ -0,0 +1,8 @@
+python ../../../malaria_amplicon_pipeline/malaria-amplicon-pipeline/ASV_to_CIGAR.py \
+  -d /Users/pam3650/Documents/Github/MHap-Analysis/reference/Pviv_P01/PvGTSeq271_refseqs.fasta \
+  --asv_to_cigar dada2/run_dada2/ASV_to_CIGAR.out.txt \
+  dada2/run_dada2/ASVSeqs.fasta \
+  dada2/run_dada2/ASVTable2.txt \
+  dada2/run_dada2/seqtab.tsv \
+  dada2/run_dada2/CIGARVariants_Bfilter.out.tsv \
+  --exclude_bimeras
