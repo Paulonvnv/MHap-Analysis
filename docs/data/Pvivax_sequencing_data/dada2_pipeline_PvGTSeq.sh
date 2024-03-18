fc01ba1164e974e80cd87e8673fcf91cca958157
Paulo Manrique <paulonvnv@gmail.com>
2024-03-18 19:32:24 -0400

    Moving Fastqs to a new path
    Fastqs of P vivax have been moved to docs/data/Pvivax_sequencing_data/Pviv_ColPerVen

--- docs/data/Pvivax_sequencing_data/dada2_pipeline_PvGTSeq.sh
+++
@@ -1,5 +0,0 @@
-Rscript ../../../malaria_amplicon_pipeline/create_meta.R -i Fastq -o PvGTSeq_metafile.csv -fw _L001_R1_001.fastq.gz -rv _L001_R2_001.fastq.gz
-mkdir dada2
-python ../../../malaria_amplicon_pipeline/malaria-amplicon-pipeline/AmpliconPipeline.py \
-  --json PvGTSeq_inputs.json
-mv prim_fq preprocess_fq run_dada2 prim_meta.txt preprocess_meta.txt stderr.txt stdout.txt ./dada2 
